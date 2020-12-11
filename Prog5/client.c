ff1e0bbd295d4f4bdc3f06e4b5b530b7394fc67d
pgunavantha <73225717+pgunavantha@users.noreply.github.com>
2020-12-11 12:38:16 +0530

    Update client.c

--- Prog5/client.c
+++ Prog5/client.c
@@ -1,69 +1,49 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <string.h>
-#include <fcntl.h>
-#include <pthread.h>
-#include <netinet/in.h>
-#include <sys/socket.h>
-#include <sys/types.h>
-#include <arpa/inet.h>
+#include<sys/socket.h>
+#include<sys/types.h>
+#include<netinet/in.h>
+#include<unistd.h>
+#include<stdlib.h>
+#include<stdio.h>
+#include<string.h>
+#include<arpa/inet.h>
 
-void * doRecieving(void * sockID){
+void str_cli(FILE *fp, int sockfd)
+{
+   int bufsize = 1024, cont;
+   char *buffer = malloc(bufsize);
+fputs("Me:",stdout);
+while(fgets(buffer,bufsize,fp)!=NULL)
+{
 
-	int clientSocket = *((int *) sockID);
+	send(sockfd, buffer, sizeof(buffer), 0);  
 
-	while(1){
-
-		char data[1024];
-		int read = recv(clientSocket,data,1024,0);
-		data[read] = '\0';
-		printf(" %s\n",data);
-
-	}
-
+  if((cont=recv(sockfd, buffer, bufsize, 0))>0) {
+	fputs("Server:",stdout);
+    fputs(buffer,stdout);
+	//bzero(buffer,10240);
+  }
+	fputs("Me:",stdout);
 }
-
-int main(){
-
-	int clientSocket = socket(PF_INET, SOCK_STREAM, 0);
-
-	struct sockaddr_in serverAddr;
-
-	serverAddr.sin_family = AF_INET;
-	serverAddr.sin_port = htons(8080);
-	serverAddr.sin_addr.s_addr = htonl(INADDR_ANY);
-
-	if(connect(clientSocket, (struct sockaddr*) &serverAddr, sizeof(serverAddr)) == -1) return 0;
-
-	printf("Connection established ............\n");
-
-	pthread_t thread;
-	pthread_create(&thread, NULL, doRecieving, (void *) &clientSocket );
-
-	while(1){
-
-		char input[1024];
-		scanf("%s",input);
-
-		if(strcmp(input,"LIST") == 0){
-
-			send(clientSocket,input,1024,0);
-
-		}
-		if(strcmp(input,"SEND") == 0){
-
-			send(clientSocket,input,1024,0);
-			
-			scanf("%s",input);
-			send(clientSocket,input,1024,0);
-			
-			scanf("%[^\n]s",input);
-			send(clientSocket,input,1024,0);
-
-		}
-
-	}
+  printf("\nEOF\n");
+}
+int main(int argc,char *argv[])
+{
+  int create_socket;
+  
+  //char fname[256];
+  struct sockaddr_in address;
+  if ((create_socket = socket(AF_INET,SOCK_STREAM,0)) > 0)
+  printf("The Socket was created\n");
+  address.sin_family = AF_INET;
+  address.sin_port = htons(16001);
+  inet_pton(AF_INET,argv[1],&address.sin_addr);
+  if (connect(create_socket,(struct sockaddr *) &address, sizeof(address)) == 0)
+  	printf("The connection was accepted with the server %s...\n",argv[1]);
+   else
+	printf("error in connect \n");
+  
+  str_cli(stdin,create_socket);
 
 
+  return close(create_socket);
 }
