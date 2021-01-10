7af2ca9d1c4c0ad1c08511503505c03e5f50c014
pgunavantha <73225717+pgunavantha@users.noreply.github.com>
2021-01-11 02:43:06 +0530

    Update checksum.c

--- Prog3/checksum/checksum.c
+++ Prog3/checksum/checksum.c
@@ -1,32 +1,38 @@
-#include<stdio.h>
-unsigned fields[10];
-unsigned short checksum()
-{
-int i;
-int sum=0;
-printf("Enter IP header information in 16 bit words\n");
-for(i=0;i<9;i++)
-{
-printf("Field %d\n",i+1);
-scanf("%x",&fields[i]);
-sum=sum+(unsigned short)fields[i];
-while (sum>>16)
-sum = (sum & 0xFFFF) + (sum >> 16);
+#include <stdio.h>
+#include <stdio.h>
+
+int checksum() {
+    unsigned int parts[9];
+    unsigned int sum;
+    printf("Enter the parts, one per line:\n");
+    for (int i = 0; i < 1; i++) {
+        scanf("%X", &parts[i]);
+        sum += parts[i];
+        while (sum >> 16)
+            sum = (sum & 0xffff) + (sum >> 16);
+    }
+    return ~sum;
 }
-sum=~sum;
-return (unsigned short)sum;
+
+int main() {
+    printf("--- SENDER ---\n");
+    int sender_checksum = checksum();
+    printf("\n--- RECV ---\n");
+    int recv_checksum = checksum();
+    printf("Sent checksum: %x\n", sender_checksum);
+    printf("Recieved checksum: %x\n", recv_checksum);
+    if (sender_checksum == recv_checksum) {
+        printf("Matching checksums!\n");
+        return 0;
+    } else {
+        printf("Mismatched checksums!\n");
+        return 0;
+    }
+
 }
-int main()
-{
-unsigned short result1, result2;
-//Sender
-result1=checksum();
-printf("\n COmputed Checksum at sender %x\n", result1);
-//Receiver
-result2=checksum();
-printf("\n COmputed Checksum at receiver %x\n", result2);
-if(result1==result2)
-printf("No error");
-else
-printf("Error in data received");
-}
+
+// 00004500 00000073 00000000 00004000 00004011 0000c0a8 00000001 0000c0a8 000000c7
+// checksum - ffffb861
+
+// 00004500 0000003c 00001c46 00004000 00004006 0000ac10 00000a63 0000ac10 00000a0c
+// checksum - 0000b1e6
