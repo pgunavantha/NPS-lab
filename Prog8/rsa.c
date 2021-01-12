5dde31f65f3748236d628e5b7bda8aa4ab5051a1
pgunavantha <73225717+pgunavantha@users.noreply.github.com>
2021-01-12 19:43:21 +0000

    Update rsa.c

--- Prog8/rsa.c
+++ Prog8/rsa.c
@@ -63,17 +63,17 @@ int main()
     do {
         d = rand() % phi;
     } while (((d * e) % phi) != 1);
-    printf( "Two prime numbers (p and q) are: %d and %d \n", p,q);   
-    int x=p*q; 
-    printf( "n(p * q) = %d * %d = %d \n", p, q, x);  
-    printf( "(p - 1) * (q - 1) = %d \n", phi);  
-    printf( "Public key (n, e): (%d , %d)\n ",n, e);
-    printf( "Private key (n, d): (%d , %d)\n ",  n, d);
+    printf( "Two prime numbers (p and q) are: %ld and %ld \n", p,q);   
+    long int x=p*q; 
+    printf( "n(p * q) = %ld * %ld = %ld \n", p, q, x);  
+    printf( "(p - 1) * (q - 1) = %ld \n", phi);  
+    printf( "Public key (n, e): (%ld , %ld)\n ",n, e);
+    printf( "Private key (n, d): (%ld , %ld)\n ",  n, d);
     for (i = 0; i < len; i++)
         cipher[i] = encrypt(text[i], n, e);
     printf( "Encrypted message: ");
     for (i = 0; i < len; i++)
-        printf( "%d", cipher[i]);
+        printf( "%ld", cipher[i]);
     for (i = 0; i < len; i++)
         text[i] = decrypt(cipher[i], n, d);
     printf("\n"); 
